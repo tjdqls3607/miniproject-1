1cdfefeb09e14ee029e54e02ba68530352381999
tjdqls3607 <tjdqls3124@gmail.com>
2025-05-14 16:22:17 +0900

    보조강사님 피드백 모두 적용

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -1,16 +1,22 @@
 async function checkLogin(){
 	const token = localStorage.getItem('token');
-	
+	const loginCheck = document.getElementById("loginCheck");
+	const btnLogin = document.getElementById("btnLogin");
+	const btnSignup = document.getElementById("btnSignup");
+	const btnLogout = document.getElementById("btnLogout");
+	const userProfileImage = document.getElementById("userProfileImage");
+
+
 	if(token){
 		document.querySelector("#userProfileImage").style.display='inline';
-			
 		document.querySelector("#btnLogin").style.display='none';
 		document.querySelector("#btnSignup").style.display='none';
+		if (btnLogout) btnLogout.style.display = "inline-block";
 	} else {
 		document.querySelector("#userProfileImage").style.display='none';
-			
 		document.querySelector("#btnLogin").style.display='inline';
 		document.querySelector("#btnSignup").style.display='inline';
+		if (btnLogout) btnLogout.style.display = "none";
 	}
 
 	document.querySelector("#loginCheck").style.display='block';	
